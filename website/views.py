c9f70f0d433846c25a6ef60db6dcb5975a728121
Sajjad Ahmadi <sajjadahmadi4sa@gmail.com>
2026-08-02 18:53:36 +0330

    create project structure

---
+++ website/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index_view (request):
+    return render (request,'index.html')
+
+def about_view (request):
+    return HttpResponse ('about page')
+
+def contact_view (request):
+    return HttpResponse ('contact page')
+
+
+# Create your views here.
