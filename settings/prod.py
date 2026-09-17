2bfbded191b1a39a45923bb7a8c449ade030042a
Sajjad Ahmadi <sajjadahmadi4sa@gmail.com>
2026-09-17 18:46:18 +0330

    Connect PostgreSQL database

--- settings/prod.py
+++ settings/prod.py
@@ -21,3 +21,12 @@ MIDDLEWARE = [
     middleware for middleware in MIDDLEWARE
     if middleware != 'debug_toolbar.middleware.DebugToolbarMiddleware'
 ]
+
+import dj_database_url
+
+DATABASES = {
+    'default': dj_database_url.config(
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
