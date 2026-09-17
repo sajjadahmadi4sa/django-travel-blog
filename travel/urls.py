d93953a524cbec62827dfb8dcef064da967832ea
Sajjad Ahmadi <sajjadahmadi4sa@gmail.com>
2026-09-17 16:26:19 +0330

    Fix travel package

---
+++ travel/urls.py
@@ -0,0 +1,21 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    
+    path('admin/', admin.site.urls),
+    path('', include('website.urls')),
+    path('blog/', include('blog.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('captcha/', include('captcha.urls')),
+    path('robots.txt', include('robots.urls')),
+]
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),] + urlpatterns
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT,)
