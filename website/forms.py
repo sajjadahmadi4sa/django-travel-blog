33cc1123424534c37874c2ef8851b3299c1b747b
Sajjad Ahmadi <sajjadahmadi4sa@gmail.com>
2026-09-19 14:18:17 +0330

    Update forms.py

--- website/forms.py
+++ website/forms.py
@@ -1,9 +1,9 @@
 from django import forms
-from captcha.fields import CaptchaField
+
 
 class ContactForm(forms.Form):
-    name = forms.CharField(max_length=100,)
+
+    name = forms.CharField(max_length=100)
     email = forms.EmailField()
-    subject = forms.CharField( max_length=200,)
+    subject = forms.CharField(max_length=200)
     message = forms.CharField()
-    captcha = CaptchaField()
