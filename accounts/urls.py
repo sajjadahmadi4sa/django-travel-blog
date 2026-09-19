04c7af35e136ec637dfc771a7bef5304d391e9db
Sajjad Ahmadi <sajjadahmadi4sa@gmail.com>
2026-09-19 15:42:42 +0330

    2

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,43 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from accounts.views import *
 
-
 app_name = 'accounts'
+
 urlpatterns = [
-    path('login',login_view,name='login'),
-    path('logout',logout_view,name='logout'),
-    path('signup',signup_view,name='signup'),
-    
+    path('login', login_view, name='login'),
+    path('logout', logout_view, name='logout'),
+    path('signup', signup_view, name='signup'),
+
+    path(
+        'password-reset',
+        auth_views.PasswordResetView.as_view(
+            template_name='accounts/password_reset_form.html'
+        ),
+        name='password_reset'
+    ),
+
+    path(
+        'password-reset/done',
+        auth_views.PasswordResetDoneView.as_view(
+            template_name='accounts/password_reset_done.html'
+        ),
+        name='password_reset_done'
+    ),
+
+    path(
+        'password-reset/<uidb64>/<token>',
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name='accounts/password_reset_confirm.html'
+        ),
+        name='password_reset_confirm'
+    ),
+
+    path(
+        'password-reset/complete',
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name='accounts/password_reset_complete.html'
+        ),
+        name='password_reset_complete'
+    ),
 ]
--- settings/prod.py
+++ settings/prod.py
@@ -16,8 +16,19 @@ INSTALLED_APPS = [
     if app != 'debug_toolbar'
 ]
 
-
 MIDDLEWARE = [
     middleware for middleware in MIDDLEWARE
     if middleware != 'debug_toolbar.middleware.DebugToolbarMiddleware'
 ]
+
+
+# Email settings - Gmail SMTP
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
